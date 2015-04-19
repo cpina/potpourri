32fc4655888ddb7e398fa695bf24a3a46c6fe68e
Carles Pina i Estany <carles@pina.cat>
2015-04-19 01:27:28 +0100

    Adds show-openvpn-traffic.py

--- show-openvpn-traffic.py
+++ show-openvpn-traffic.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python
 
+"""
+From a log file with the OpenVPN connection lines like:
+
+Apr 14 23:18:31 localhost openvpn: Session $Client TX: 592794812 bytes RX: 42195316 bytes Duration: 11679 seconds HostIP: $IP
+
+shows nicer lines like:
+
+Apr 14 23:18:31 $Client TX: 565.333187 MB RX: 40.240589 MB 194 min. $IP
+"""
+
 import re
 
 f = open("/var/log/openvpn-traffic.log")
@@ -13,7 +23,7 @@ def format_bytes(b):
 for line in f.readlines():
     line = line.rstrip()
     m = re.match("(.*) localhost.*Session ([^ ]+) TX: ([0-9]+) bytes RX: ([0-9]+) bytes Duration: ([0-9]+) seconds HostIP: ([0-9\.]+).*",line)
-	
+
     if m:
         date=m.group(1)
         user=m.group(2)
